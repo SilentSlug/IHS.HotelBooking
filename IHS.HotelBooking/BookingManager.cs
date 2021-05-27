8d078e5d521aef9a76ab6820b553f9628dfa4831
Joseph South <josephwilliamsouth@outlook.com>
2021-05-27 14:25:44 +0100

    Added Booking model and BookingManager class

---
+++ IHS.HotelBooking/BookingManager.cs
@@ -0,0 +1,19 @@
+﻿using IHS.HotelBooking.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace IHS.HotelBooking
+{
+    public class BookingManager : IBookingManager
+    {
+        public async Task AddBooking(string guest, int room, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> IsRoomAvailable(int room, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IHS.HotelBooking/Models/Booking.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace IHS.HotelBooking.Models
+{
+    public class Booking
+    {
+        public string CustomerSurname { get; set; }
+
+        public int RoomNumber { get; set; }
+
+        public DateTime BookingDate { get; set; }
+    }
+}
