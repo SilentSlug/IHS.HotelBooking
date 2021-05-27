850101b93ea76e5d6496d6cc734a8d6be6be3189
Joseph South <josephwilliamsouth@outlook.com>
2021-05-27 15:56:39 +0100

    Implemented IsRoomAvailable method

--- IHS.HotelBooking/BookingManager.cs
+++ IHS.HotelBooking/BookingManager.cs
@@ -1,19 +1,50 @@
 ﻿using IHS.HotelBooking.Interfaces;
+using IHS.HotelBooking.Models;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace IHS.HotelBooking
 {
     public class BookingManager : IBookingManager
     {
+        private List<Booking> bookings;
+
+        public BookingManager()
+        {
+            bookings = new List<Booking>();
+        }
+
         public async Task AddBooking(string guest, int room, DateTime date)
         {
-            throw new NotImplementedException();
+            try
+            {
+
+            }
+            catch
+            {
+
+            }
         }
 
         public async Task<bool> IsRoomAvailable(int room, DateTime date)
         {
-            throw new NotImplementedException();
+            try
+            {
+                bool availability = false;
+
+                await Task.Run(() =>
+                {
+                    availability = !bookings.Any(b => b.RoomNumber == room && b.BookingDate == date);
+                }).ConfigureAwait(false);
+
+                return availability;
+            }
+            catch
+            {
+                throw new Exception("An unexpected error occured when checking room availability.");
+            }
         }
     }
 }
