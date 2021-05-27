6820278db1ec281b8f9da2ddc42eff0c889c1330
Admin <Admin@DESKTOP-39HQU04>
2021-05-27 13:51:42 +0100

    Add project files.

---
+++ IHS.HotelBooking/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IHS.HotelBooking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
