3484223b864aab92215fb4a582ae54ac553d72a8
Joseph South <josephwilliamsouth@outlook.com>
2021-05-28 10:29:53 +0100

    Implement test code for GetAvailableRooms

--- IHS.HotelBooking/Program.cs
+++ IHS.HotelBooking/Program.cs
@@ -13,18 +13,40 @@ namespace IHS.HotelBooking
 
             var today = DateTime.Today;
 
+            //Get all available rooms
+            var availableRooms = await bookingManager.GetAvailableRooms(today).ConfigureAwait(false);
+
+            //Display available rooms to console
+            foreach (var room in availableRooms)
+            {
+                Console.WriteLine(room.ToString());
+            }
+
+            //Check if room 101 is available
             await Task.Run(() =>
             {
                 Console.WriteLine(bookingManager.IsRoomAvailable(101, today).Result);
             }).ConfigureAwait(false);
 
+            //Add booking for room 101
             await bookingManager.AddBooking("South", 101, today).ConfigureAwait(false);
 
+            //Check if room 101 is available after booking
             await Task.Run(() =>
             {
                 Console.WriteLine(bookingManager.IsRoomAvailable(101, today).Result);
             }).ConfigureAwait(false);
 
+            //Check all available rooms after booking
+            availableRooms = await bookingManager.GetAvailableRooms(today).ConfigureAwait(false);
+
+            //Display available rooms to console
+            foreach (var room in availableRooms)
+            {
+                Console.WriteLine(room.ToString());
+            }
+
+            //Attempt to add booking for an unavailable room
             await bookingManager.AddBooking("South", 101, today).ConfigureAwait(false);
 
             Console.Read();
